6e655e02e0a521d4769e2b9b87ff3005bfd5bf6d
JFRode <joaofelipe.rode@gmail.com>
2016-03-21 16:08:18 -0300

    Ajuste de telas
    Ajustado telas de Login e Cadastro.
    Criado transição entre Login e Cadastro.

--- app/src/main/java/br/com/waypoints/waypoints/MainActivity.java
+++ app/src/main/java/br/com/waypoints/waypoints/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.waypoints.waypoints;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView cadastro = (TextView) findViewById(R.id.textViewCadastro);
+
+        cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityCadastro = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(activityCadastro);
+            }
+        });
     }
+
+
 }

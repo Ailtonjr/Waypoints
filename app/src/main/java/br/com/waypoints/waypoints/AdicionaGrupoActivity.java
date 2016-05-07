9586aea2a52626022acbfaa158842bc6bb64800f
JFRode <joaofelipe.rode@gmail.com>
2016-05-07 15:16:11 -0300

    Reparado problema de tela no grupos activity. radioButton ok, com metodo para capturar a string do selecionado.

--- app/src/main/java/br/com/waypoints/waypoints/AdicionaGrupoActivity.java
+++ app/src/main/java/br/com/waypoints/waypoints/AdicionaGrupoActivity.java
@@ -3,18 +3,62 @@ package br.com.waypoints.waypoints;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
 
 public class AdicionaGrupoActivity extends AppCompatActivity {
+
+    private RadioButton radioButtonPlanejador;
+    private RadioButton radioButtonEntregador;
+    private View view;
+    private ImageButton imageButtonAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adiciona_grupo);
+
+        radioButtonPlanejador = (RadioButton) findViewById(R.id.radioButtonPlanejadorAdicionaGrupo);
+        radioButtonEntregador = (RadioButton) findViewById(R.id.radioButtonEntregadorAdicionaGrupo);
+        imageButtonAdd = (ImageButton) findViewById(R.id.imageButtonAddAdicionaGrupo);
+
+        imageButtonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //  Botao adiciona integrante. Adicionar a uma lista antes de integrar o grupo por completo.
+            }
+        });
+
+        radioButtonPlanejador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioButtonEntregador.isChecked()) radioButtonEntregador.setChecked(false);
+                view = v;
+            }
+        });
+
+        radioButtonEntregador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (radioButtonPlanejador.isChecked()) radioButtonPlanejador.setChecked(false);
+                view = v;
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.botao_ok, menu);
         return true;
+    }
+
+    private String tipoIntegrante() {
+        if(radioButtonEntregador.isChecked()){
+            return "Entregador";
+        }else if (radioButtonPlanejador.isChecked()){
+            return "Planejador";
+        }
+        return null;
     }
 }

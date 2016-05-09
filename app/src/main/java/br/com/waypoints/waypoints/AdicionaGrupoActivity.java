afbdf935729d10ee3b882a59aa1f80856512723a
JFRode <joaofelipe.rode@gmail.com>
2016-05-09 00:39:46 -0300

    Implementado lista de emails dos integrantes na criação do grupo.

--- app/src/main/java/br/com/waypoints/waypoints/AdicionaGrupoActivity.java
+++ app/src/main/java/br/com/waypoints/waypoints/AdicionaGrupoActivity.java
@@ -4,15 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdicionaGrupoActivity extends AppCompatActivity {
 
     private RadioButton radioButtonPlanejador;
     private RadioButton radioButtonEntregador;
     private View view;
+    private EditText editTextEmail;
     private ImageButton imageButtonAdd;
+    private ListView listViewEmails;
+    private List<String> listEmails = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +33,18 @@ public class AdicionaGrupoActivity extends AppCompatActivity {
 
         radioButtonPlanejador = (RadioButton) findViewById(R.id.radioButtonPlanejadorAdicionaGrupo);
         radioButtonEntregador = (RadioButton) findViewById(R.id.radioButtonEntregadorAdicionaGrupo);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmailAdicionaGrupo);
         imageButtonAdd = (ImageButton) findViewById(R.id.imageButtonAddAdicionaGrupo);
+        listViewEmails = (ListView) findViewById(R.id.listViewEmailAdicionaGrupo);
+        adapter = new ArrayAdapter(this, R.layout.itemlist_email, R.id.textViewEmailItemList);
+        listViewEmails.setAdapter(adapter);
 
         imageButtonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //  Botao adiciona integrante. Adicionar a uma lista antes de integrar o grupo por completo.
+                addListItem(editTextEmail.getText().toString());
+                editTextEmail.setText("");
             }
         });
 
@@ -45,6 +63,12 @@ public class AdicionaGrupoActivity extends AppCompatActivity {
                 view = v;
             }
         });
+    }
+
+    public void addListItem(String email) {
+        listEmails.add(email);
+        adapter.add(email);
+        adapter.notifyDataSetChanged();
     }
 
     @Override
